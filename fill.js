9a7f9b2a91f687055e9c0d1a05051b3b73c41ebc
Bob Hunt <bob.hunt.email@gmail.com>
2018-03-20 15:00:24 -0400

    fill is working

--- fill.js
+++ fill.js
@@ -11,32 +11,49 @@ Bitmap.prototype.fill = function(row, col, new_color) {
     //
     //
     // Push the coordinates [row, col] onto the queue.
-    queue.push(row, col);
-    console.log(queue);
+    queue.push(row);
+    queue.push(col);
+    // console.log(queue);
     // While the queue is not empty:
     while(queue.length>0){
-        var y=queue.shift();
-        var x=queue.shift();
-        console.log("x = " + x + " y= " + y);
+        //    Shift a pair of coordinates [r,c] off the front of the queue.
+        var tmpRow=queue.shift();
+        var tmpCol=queue.shift();
         
-        if (originalBoard[west][y] === originalColor) {
-            originalBoard[west][y] = newColor;
-            if (originalBoard[west][north] === originalColor) {
-                newBoard.row.push(west);
-                newBoard.column.push(north);
-                originalBoard[west][north] = newColor;
-            } else if (originalBoard[west][south] === originalColor) {
-                newBoard.row.push(west);
-                newBoard.column.push(south);
-                originalBoard[west][south] = newColor;
-            }
+        //    The 4-connected neighbors are the cells above, below, left, and right.
+        this.setColor(tmpRow, tmpCol, new_color);
+        var originalBoard=this.grid;
+        var north=tmpRow-1;
+        var south=tmpRow+1;
+        var west=tmpCol-1;
+        var east=tmpCol+1;
+        
+        if (originalBoard[north][tmpCol] === old_color) {
+            queue.push(north);
+            queue.push(tmpCol);       
+        }
+            
+        if (originalBoard[south][tmpCol] === old_color) {
+            queue.push(south);
+            queue.push(tmpCol); 
+        }
+                
+        if (originalBoard[tmpRow][west] === old_color) {
+            queue.push(tmpRow);
+            queue.push(west); 
+        }
+
+        if (originalBoard[tmpRow][east] === old_color) {
+            queue.push(tmpRow);
+            queue.push(east); 
+        }
+        
         }
     }
-    //    Shift a pair of coordinates [r,c] off the front of the queue.
-    //    The 4-connected neighbors are the cells above, below, left, and right.
+    
+    
     //    Check each of those 4 neighbors:
     //       If the neighbor is old_color:
     //          Set the neighbor to new_color.
     //          Add the neighbors coordinates to the queue
     //          (to ensure we later check its neighbors as well).
-}
